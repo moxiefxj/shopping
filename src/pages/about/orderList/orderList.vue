cbc92987c325977aa1cf3efb0cada40a38fc706b
青辰 <32487657+moxiefxj@users.noreply.github.com>
2020-06-09 00:07:52 +0800

    滑动加载下一页

--- src/pages/about/orderList/orderList.vue
+++ src/pages/about/orderList/orderList.vue
@@ -1,6 +1,6 @@
 <template>
     <view v-if="hasOrder">
-        <view v-for="(item,index) in orderlist" :key="index" @click="toProduct(item.business_id)">
+        <view id="listOrder" v-for="(item,index) in orderlist" :key="index" @click="toProduct(item.business_id)">
             <order-com :item="item" :order_del = "order_del" ></order-com>
         </view>
         
@@ -20,7 +20,13 @@ export default {
     data() {
         return {
             orderlist:[],
-            hasOrder:false
+            hasOrder:false,
+            totalPage:0,
+            presentPage:0,
+
+            screenHeight:0,
+            isLoading:false,
+            bottomDistinct:200,
         }
     },
     beforeMount() {
@@ -29,6 +35,8 @@ export default {
             u_id:this.$store.state.userInfo.id
         }).then( res => {
             this.orderlist = res.data.data
+            this.totalPage = res.data.totalPage
+            this.presentPage = 1
             if(this.orderlist == null){
                 this.hasOrder = false
             }else{
@@ -38,7 +46,7 @@ export default {
                 });
             }
            
-            console.log(res)
+            // console.log(res)
         }).catch( res => {
             console.log(res)
         })
@@ -76,6 +84,71 @@ export default {
                 },
                 complete: () => {}
             })
+        },
+        onPageScroll : function(e){
+            const scrollTop = e.scrollTop;
+            
+            // console.log(scrollTop)
+            // 防止重复触发
+            if(this.isLoading){
+                return;
+            }
+
+            // 获取SelectorQuery 对象示例
+            const query = uni.createSelectorQuery().in(this)
+
+            // 为listArea 节点绑定查询请求
+            query.select("#listOrder").boundingClientRect(data => {
+                let height = data.height;
+                if(this.bottomDistinct >= height - this.screenHeight - scrollTop){
+                    console.log("hhhh")
+                    this.isLoading = true
+                    setTimeout(()=>{
+                        // 判断totalPage的值
+                        if(this.presentPage >= this.totalPage){
+                            uni.showToast({
+                                title: '没有更多啦',
+                                duration: 1000,
+                                icon:"none"
+                            });
+                        }
+                        else{
+                            // 加载下一页
+                            let url = '/order/?page='+(this.presentPage+1)
+                            this.$uniRequest.post(url,{
+                                u_id:this.$store.state.userInfo.id
+                            }).then( res => {
+                                console.log(res)
+                                let list = res.data.data
+                                list.forEach(element => {
+                                    element.cover_img = this.$imageUrl + element.cover_img
+                                });
+                                this.orderlist = this.orderlist.concat(list)
+                            })
+                        }
+                    //     if(this.next_page_url == null){
+                    //         uni.showToast({
+                    //             title: '没有更多啦',
+                    //             duration: 1000,
+                    //             icon:"none"
+                    //         });
+                    //     }else{
+                    //         // 获取下一页数据
+                    //         this.$uniRequest.get(this.next_page_url).then( res => {
+                    //             // console.log(res)
+                    //             this.next_page_url = res.data.next_page_url
+                    //             res.data.data.forEach(element => {
+                    //                 element.cover_img = this.$imageUrl + element.cover_img 
+                    //             });
+                    //             this.productlist = this.productlist.concat(res.data.data)
+                    //         }).catch( res => {
+                    //             console.log(res)
+                    //         })
+                    //     }
+                    //     this.isLoading = false
+                    },300);
+                }
+            }).exec();
         }
     }
 }
--- src/pages/tabBar/index/index.vue
+++ src/pages/tabBar/index/index.vue
@@ -1,11 +1,14 @@
 <template>
     <view class="pages">
         <!--搜索框  -->
-        <uni-search-bar></uni-search-bar>
+        <uni-search-bar @confirm="search"></uni-search-bar>
         <!-- 导航栏 -->
         <ms-tabs :list="itemBar" v-model="active2" itemColor="#03A9F4" lineColor="#03A9F4" >
         </ms-tabs>
-        <product-view :productlist = "productlist" ></product-view>
+        <view id = "listArea">
+            <product-view :productlist = "productlist" ></product-view>
+        </view>
+        
         <!-- 内容 -->
         
           
@@ -39,8 +42,15 @@ export default {
             ],
             active2:0,
             productlist:[],
-            categories:""
+            categories:"",
+            screenHeight:0,
+            isLoading:false,
+            bottomDistinct:200, //距离底部多少像素触发
+            next_page_url:'',
         }
+    },
+    onLoad(){
+        this.screenHeight = uni.getSystemInfoSync().screenHeight
     },
     beforeMount() {
         this.setProductlist(this.active2) 
@@ -69,11 +79,12 @@ export default {
             if(this.categories == "all"){
                 this.$uniRequest.get('/Business').then(res => {
                     this.productlist = res.data.data
+                    this.next_page_url = res.data.next_page_url
                     // 修改iamge路径
                     this.productlist.forEach(element => {
                         element.cover_img = this.$imageUrl + element.cover_img 
                     });
-                    console.log(this.productlist)
+                    // console.log(res)
                 }).catch(res => {
                     console.log(res)
                 })
@@ -83,11 +94,12 @@ export default {
                     categories:this.categories
                 }).then(res => {
                     this.productlist = res.data.data 
+                    this.next_page_url = res.data.next_page_url
                     // 修改iamge路径
                     this.productlist.forEach(element => {
                         element.cover_img = this.$imageUrl + element.cover_img 
                     });
-                    console.log(this.productlist)
+                    // console.log(this.productlist)
                 }).catch(res => {
                     console.log(res)
                 })
@@ -95,7 +107,58 @@ export default {
             
             
             
-        }
+        },
+        onPageScroll : function(e){
+            const scrollTop = e.scrollTop;
+            
+            // console.log(scrollTop)
+            // 防止重复触发
+            if(this.isLoading){
+                return;
+            }
+
+            // 获取SelectorQuery 对象示例
+            const query = uni.createSelectorQuery().in(this)
+
+            // 为listArea 节点绑定查询请求
+            query.select("#listArea").boundingClientRect(data => {
+                let height = data.height;
+                if(this.bottomDistinct >= height - this.screenHeight - scrollTop){
+                    this.isLoading = true
+                    setTimeout(()=>{
+                        if(this.next_page_url == null){
+                            uni.showToast({
+                                title: '没有更多啦',
+                                duration: 1000,
+                                icon:"none"
+                            });
+                        }else{
+                            // 获取下一页数据
+                            this.$uniRequest.get(this.next_page_url).then( res => {
+                                // console.log(res)
+                                this.next_page_url = res.data.next_page_url
+                                res.data.data.forEach(element => {
+                                    element.cover_img = this.$imageUrl + element.cover_img 
+                                });
+                                this.productlist = this.productlist.concat(res.data.data)
+                            }).catch( res => {
+                                console.log(res)
+                            })
+                        }
+                        this.isLoading = false
+                    },300);
+                }
+            }).exec();
+        },
+        search:function(e){
+            this.$uniRequest.post('/BusinessSearch',{
+                key_word : e.value
+            }).then( res => {
+                console.log("搜索结果：")
+                console.log(res)
+            })
+        }   
+
     },
     watch: {
         active2(ol,val){
